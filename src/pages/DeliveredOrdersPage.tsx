d7dc44d88b22b8ec159535339a2132794afc35bd
mounirmslamani-ui <mounir.m.slamani@gmail.com>
2026-05-23 19:49:52 +0100

    Update DeliveredOrdersPage.tsx

--- src/pages/DeliveredOrdersPage.tsx
+++ src/pages/DeliveredOrdersPage.tsx
@@ -140,7 +140,8 @@ const DeliveredOrdersPage: React.FC = () => {
           }
         />
 <div className="flex justify-end mb-2">
-  <Tabs value={activeCat} onValueChange={(v) => setActiveCat(v as OrderCategory)} className="flex-none">
+<Tabs value={activeCat} onValueChange={(v) => setActiveCat(v as OrderCategory)}>
+  <div className="flex justify-end mb-2">
     <TabsList>
       {(['fabrication','prestation','divers','slamani'] as OrderCategory[]).map(c => (
         <TabsTrigger key={c} value={c}>
@@ -149,8 +150,8 @@ const DeliveredOrdersPage: React.FC = () => {
         </TabsTrigger>
       ))}
     </TabsList>
-  </Tabs>
-</div>
+  </div>
+</Tabs>
 
       <div className="min-h-0 flex-1 overflow-auto rounded-lg border bg-card">
         <Table>
